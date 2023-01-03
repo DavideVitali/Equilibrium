383ffd3d89414e77082661163327feb2072a1b50
DavideVitali <davide.bass@gmail.com>
2023-01-03 18:25:50 +0100

    Prima modifica dell'interfaccia per l'ispezione del modello del filtro

--- Equilibrium/Components/SortableTable/ISortableTableFilter.cs
+++ Equilibrium/Components/SortableTable/ISortableTableFilter.cs
@@ -2,12 +2,19 @@
 
 namespace Equilibrium.Components.SortableTable
 {
-    public interface ISortableTableFilter
+    /// <summary>
+    /// Defines a contract for a SortableTable filter that automatically creates a model
+    /// based on the <see cref="TTargetEntity"/> instance.
+    /// </summary>
+    /// <typeparam name="TTargetEntity">An instance of the Entity that will hold the filter keys and values</typeparam>
+    public interface ISortableTableFilter<TTargetEntity> where TTargetEntity : class, new()
     {
         /// <summary>
         /// Json DOM representing the fields selected as filter
         /// </summary>
         public string JsonDOMContainer { get; }
+
+        Dictionary<string, IEnumerable<string>> FilterModel { get; }
 
         //public static Dictionary<object, List<object>> CreateFilterModel();
     }
